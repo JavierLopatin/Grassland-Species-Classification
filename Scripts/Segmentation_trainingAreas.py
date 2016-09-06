30742f940722c977d079b7cbdc15e8d7f5e49e33
JavierLopatin <javierlopatin@gmail.com>
2016-09-06 14:29:38 +0200

    commit2

--- Scripts/Segmentation_trainingAreas.py
+++ Scripts/Segmentation_trainingAreas.py
@@ -8,23 +8,17 @@ from rsgislib.segmentation import segutils
 # Create a list of rasters
 rasterList = glob("*.tif")
 # Delete all images of the plots and leav only the ones from the calibration samples
-rasterList = [x for x in rasterList if x is not "plot"]  
+rasterList = [x for x in rasterList if "plot" not in x]  
 
 # Create folders to store results if thay do no exist
-if not os.path.exists("BN_MNF"):
-    os.makedirs("BN_MNF")
+if not os.path.exists("clumps"):
+    os.makedirs("clumps")
 
-if not os.path.exists("MNF"):
-    os.makedirs("MNF")
+if not os.path.exists("shp"):
+    os.makedirs("shp")
 
 
 ##################### Perform Segmentation #####################
-# The input image for the segmentation
-inputImage = "N06W053_96-10_stack_lee_dB.kea"
-# The output segments (clumps) image
-segmentClumps = "N06W053_96-10_segs.kea"
-# The output clump means image (for visualsation) 
-outputMeanSegments = "N06W053_96-10_meansegs.kea"
 # A temporary path for layers generated during the
 # segmentation process. The directory will be created
 # and deleted during processing.
@@ -35,14 +29,22 @@ numClusters = 30
 minObjectSize = 50
 # The distance threshold to prevent merging.
 # this has been set to an arbitrarily large 
-# number to disable this function. 
+# number to disable this function.  
 distThres = 1000000
 # The sampling of the input image for input
 # to the KMeans
 imgSampling = 100
 # Maximum number of iterations within KMeans
 maxKMeanIter = 200
+
 # RSGISLib function call to execute the segmentation
-segutils.runShepherdSegmentation(inputImage, segmentClumps, outputMeanSegments, 
-                                 tmpPath, "GTiff", False, False, False, numClusters, 
-                                 minObjectSize, distThres, None, imgSampling, maxKMeanIter)
+for i in range(len(rasterList)):
+    # The input image for the segmentation
+    inputImage = rasterList[i]
+    # The output segments (clumps) image
+    segmentClumps = "/clumps/"+rasterList[i][:-4]+"clumps.tif"
+    # The output clump means image (for visualsation) 
+    outputMeanSegments = "/clumps/"+rasterList[i][:-4]+"segments.tif"
+    segutils.runShepherdSegmentation(inputImage, segmentClumps, outputMeanSegments, 
+                                     tmpPath, "GTiff", False, False, False, numClusters, 
+                                     minObjectSize, distThres, None, imgSampling, maxKMeanIter)
