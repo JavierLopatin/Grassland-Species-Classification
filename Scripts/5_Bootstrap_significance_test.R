6d6e31406bbb4f7e061bb7cbbccf3b5ef17d9694
JavierLopatin <javierlopatin@gmail.com>
2016-11-15 16:01:27 +0100

    Boots test

---
+++ Scripts/5_Bootstrap_significance_test.R
@@ -0,0 +1,56 @@
+
+##################################
+## Bootstrap significance test ###
+##################################
+
+### Leaf level 
+
+boot_test <- significanceTest_LeafLevel(data, fitASD, fitAISA)
+save(boot_test, file="boot_testLeaf.Rdata")
+
+# Hist do not have to overlap with 0, otherwise is not significant
+# The black "zero-line" needs to be left of the blue "alpha-line". The green line is just the upper quantile.
+par(mfrow=c(1,2), mar=c(2,3,3,1))
+main <- c("OA Leaf level", "Kappa Leaf level")
+for(i in 1:2){
+  hist(unlist(boot_test$boot_test[i]), main=main[i], col="grey", border="white", xlab="", ylab="")
+  abline(v=quantile(unlist(boot_test$boot_test[i]), probs=c(0.05, 0.95)), col=c("blue", "green"))
+  abline(v=0, col=c("black"))
+  box()
+}
+
+save.image("ClassLeafLevel.RData")
+
+### Canopy level
+
+dat_potVal <- read.table("D:/Sp_Images/BootsClass_out/Fits_all_pot.txt", header = T)
+dat_rfVal  <- read.table("D:/Sp_Images/BootsClass_out/Fits_all_rf.txt", header = T)
+
+sig_canopy <- significanceTest_CanopyLevel(dat_rfVal, dat_potVal)
+
+# Hist do not have to overlap with 0, otherwise is not significant
+# The black "zero-line" needs to be left of the blue "alpha-line". The green line is just the upper quantile.
+par(mfrow=c(3,2), mar=c(2,3,3,1))
+main <- names(sig_canopy)
+for(i in 1:length(sig_canopy)){
+  hist(unlist(sig_canopy[i]), main=main[i], col="grey", border="white", xlab="", ylab="")
+  abline(v=quantile(unlist(sig_canopy[i]), probs=c(0.05, 0.95)), col=c("blue", "green"))
+  abline(v=0, col=c("black"))
+  box()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
