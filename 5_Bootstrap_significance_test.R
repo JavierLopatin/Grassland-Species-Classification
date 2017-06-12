1636b1882e39ab763240907802ea0dc18c9ea98d
Javier Lopatin <javierlopatin@gmail.com>
2017-06-12 11:24:21 +0200

    Create 5_Bootstrap_significance_test.R

--- 5_Bootstrap_significance_test.R
+++ 5_Bootstrap_significance_test.R
@@ -4,32 +4,13 @@
 ## author: Javier Lopatin                                                     ##
 ## mail: javierlopatin@gmail.com                                              ##  
 ##                                                                            ##
-## Manuscript: Hyperspectral classification of grassland species: towards an  ##
-##             UAS application for semi-automatic field surveys               ##
+## Manuscript: Mapping plant species in mixed grassland communities using     ##
+##             close range imaging spectroscopy                               ##
 ##                                                                            ##
 ## description: One-sided bootstrap significance test                         ## 
 ##                                                                            ##
 ################################################################################
 
-### Leaf level 
-
-boot_test <- significanceTest_LeafLevel(data, fitASD, fitAISA)
-save(boot_test, file="boot_testLeaf.Rdata")
-
-# Hist do not have to overlap with 0, otherwise is not significant
-# The black "zero-line" needs to be left of the blue "alpha-line". The green line is just the upper quantile.
-par(mfrow=c(1,2), mar=c(2,3,3,1))
-main <- c("OA Leaf level", "Kappa Leaf level")
-for(i in 1:2){
-  hist(unlist(boot_test$boot_test[i]), main=main[i], col="grey", border="white", xlab="", ylab="")
-  abline(v=quantile(unlist(boot_test$boot_test[i]), probs=c(0.05, 0.95)), col=c("blue", "green"))
-  abline(v=0, col=c("black"))
-  box()
-}
-
-save.image("ClassLeafLevel.RData")
-
-### Canopy level
 
 dat_potVal <- read.table("D:/Sp_Images/BootsClass_out/Fits_all_pot.txt", header = T)
 dat_rfVal  <- read.table("D:/Sp_Images/BootsClass_out/Fits_all_rf.txt", header = T)
